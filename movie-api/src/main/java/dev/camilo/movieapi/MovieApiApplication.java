b00bc7dc570c01c668f0d4e4d76e58e34694bdac
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2024-02-29 22:29:57 -0500

    Starting project

---
+++ movie-api/src/main/java/dev/camilo/movieapi/MovieApiApplication.java
@@ -0,0 +1,13 @@
+package dev.camilo.movieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//anotations work to get know the compiler what the class does
+@SpringBootApplication
+public class MovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApiApplication.class, args);
+	}
+
+}
